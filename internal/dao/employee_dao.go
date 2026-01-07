94b50f7799111ab21344f6779926c7607091e241
mahigadamsetty <80254540+mahigadamsetty@users.noreply.github.com>
2026-01-07 15:29:38 +0530

    chore: emlpoyee dao create implementation

---
+++ internal/dao/employee_dao.go
@@ -0,0 +1,40 @@
+package dao
+
+import (
+	"database/sql"
+
+	"test-assignment/internal/model"
+)
+
+type EmployeeRepository struct {
+	db *sql.DB
+}
+
+func NewEmployeeRepository(db *sql.DB) *EmployeeRepository {
+	return &EmployeeRepository{db: db}
+}
+
+func (r *EmployeeRepository) Create(emp *model.Employee) error {
+	query := `
+	INSERT INTO employees (first_name, last_name, job_title, salary)
+	VALUES (?, ?, ?, ?)
+	`
+
+	result, err := r.db.Exec(
+		query,
+		emp.FirstName,
+		emp.LastName,
+		emp.JobTitle,
+		emp.Salary,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
