b0ff273b63685566ecc429e679ae555ee2193eec
mahigadamsetty <80254540+mahigadamsetty@users.noreply.github.com>
2026-01-07 13:57:28 +0530

    Refactor: Added validation checks

--- internal/service/employee_service.go
+++ internal/service/employee_service.go
@@ -1,6 +1,14 @@
 package service
 
-import "test-assignment/internal/model"
+import (
+	"errors"
+	"strings"
+
+	"test-assignment/internal/model"
+)
+
+var ErrInvalidEmployee = errors.New("invalid employee data")
+var ErrEmployeeNotFound = errors.New("employee not found")
 
 type EmployeeServiceInterface interface {
 	CreateEmployee(employee model.Employee) error
@@ -13,5 +21,22 @@ func NewEmployeeService() *employeeService {
 }
 
 func (s *employeeService) CreateEmployee(employee model.Employee) error {
+	if strings.TrimSpace(employee.FirstName) == "" {
+		return ErrInvalidEmployee
+	}
+
+	if strings.TrimSpace(employee.LastName) == "" {
+		return ErrInvalidEmployee
+	}
+
+	if strings.TrimSpace(employee.JobTitle) == "" {
+		return ErrInvalidEmployee
+	}
+
+	if employee.Salary <= 0 {
+		return ErrInvalidEmployee
+	}
+
+	// DAO call will come later
 	return nil
 }
