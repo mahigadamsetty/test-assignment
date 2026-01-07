c4774cb98ed84b531e502b31c9044502195c29b6
mahigadamsetty <80254540+mahigadamsetty@users.noreply.github.com>
2026-01-07 10:20:42 +0530

    test: add service test for successful create employee

--- internal/service/employee_service_test.go
+++ internal/service/employee_service_test.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"test-assignment/internal/model"
+	"testing"
+)
+
+func Test_EmployeeService_CreateEmployee_Success(t *testing.T) {
+	service := NewEmployeeService()
+
+	employee := model.Employee{
+		FirstName: "Mahi",
+		LastName:  "Kumar",
+		JobTitle:  "Developer",
+		Salary:    70000,
+	}
+
+	err := service.CreateEmployee(employee)
+	if err != nil {
+		t.Errorf("CreateEmployee() error = %v, want nil", err)
+	}
+}
